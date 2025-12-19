e147798cfe940f84f9d6e68a695f821a5d358570
Aryan214G <aryan214gm@gmail.com>
2025-12-19 11:14:17 +0530

    feat(frontend): Add routing for Home and Auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
-    return <HomePage/> 
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path = '/Auth' element = {<AuthPage/>}></Route>
+            </Routes>
+        </BrowserRouter> 
+    )
+    
 }
 
 export default App
